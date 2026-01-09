17c24179ef64cbea809cd66213408ef5bb9b1aeb
Mandarisse <moahmedmandarisse@gmail.com>
2026-01-09 15:38:48 +0100

    services creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
+import { Services } from "./components/Services";
 
 function App() {
   return (
     <>
       <Header />
+      <Services/>
 
       <Routes>
-        <Route path="/" element={<div style={{ padding: "120px 40px" }}>Home page (vide pour l’instant)</div>} />
+        <Route path="/" />
         <Route path="/services" element={<div style={{ padding: "120px 40px" }}>Services page</div>} />
        
        
         <Route path="/contact" element={<div style={{ padding: "120px 40px" }}>Contact page</div>} />
       </Routes>
+      
     </>
   );
 }
